312c8658b674a2d65371ec430ba0d6a1d75ad5f9
jeninpj <jeninpj52@gmail.com>
2026-07-18 16:04:57 +0530

    Fix superuser migration

--- app/migrations/0004_create_superuser.py
+++ app/migrations/0004_create_superuser.py
@@ -21,7 +21,7 @@ def remove_superuser(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0003_chatmessage'),  
+        ('app', '0003_chatmessage'),  # ← Your previous migration
         ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
